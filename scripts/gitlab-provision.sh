07b4b4632ff8b01dc3dfa512c0227938863c5633
Lokaler Administrator <lokaleradmin@R504U.training.erfurt.iad.de>
2020-12-10 08:15:45 +0100

    Update

--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -7,7 +7,7 @@ curl -fsSl https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -    
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" #Downloads the docker
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io #install the downloaded Docker
-#to shows the version of Docker: apt-cache madison docker-ce
+#to check the version of Docker: apt-cache madison docker-ce
 usermod -aG docker vagrant
 systemctl enable docker #enable our docker
 #to check the docker response use: vagrant@gitlab:~$ docker run hello-world
